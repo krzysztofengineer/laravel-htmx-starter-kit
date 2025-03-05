49c078822dbd381af4874bb3ba3a28f9e76133c1
Krzysztof <krzysztof@engineer.com>
2025-03-05 14:22:08 +0100

    dashboard test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::view('/dashboard', 'dashboard');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
