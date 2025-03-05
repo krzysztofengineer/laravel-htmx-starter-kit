8baaf9b46547e332c96e8bc5bb8795097011395d
Krzysztof <krzysztof@engineer.com>
2025-03-05 20:49:21 +0100

    email verification test

--- routes/auth.php
+++ routes/auth.php
@@ -1,13 +1,18 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::view('/login', 'login')->name('login');
+    Route::view('/login', 'auth.login')->name('login');
     Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 });
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+    Route::view('/verify-email', 'auth.verify-email')->name('verification.notice');
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
 });
