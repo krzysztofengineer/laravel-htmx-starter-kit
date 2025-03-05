611ed36000525face7c8191b5224c051d8b8224c
Krzysztof <krzysztof@engineer.com>
2025-03-05 21:25:16 +0100

    registration test

--- routes/auth.php
+++ routes/auth.php
@@ -4,10 +4,13 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
+    Route::view('register', 'auth.register')->name('register');
+    Route::post('register', [RegisteredUserController::class, 'store']);
     Route::view('/login', 'auth.login')->name('login');
     Route::post('/login', [AuthenticatedSessionController::class, 'store']);
     Route::view('forgot-password', 'auth.forgot-password')->name('password.request');
