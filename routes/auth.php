1948ccfcfb9568f49821e39dbaca1fc95188af9e
Krzysztof <krzysztof@engineer.com>
2025-03-05 21:14:41 +0100

    password confirmation test

--- routes/auth.php
+++ routes/auth.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,7 +13,9 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
     Route::view('/verify-email', 'auth.verify-email')->name('verification.notice');
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+    Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
+    Route::view('confirm-password', 'auth.confirm-password')->name('password.confirm');
+    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 });
