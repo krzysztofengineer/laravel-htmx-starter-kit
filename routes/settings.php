a8a6f04c27d005a732c7dc61b6061355cc2d8376
Krzysztof <krzysztof@engineer.com>
2025-03-07 09:56:47 +0100

    settings tests

--- routes/settings.php
+++ routes/settings.php
@@ -1,2 +1,18 @@
 <?php
 
+use App\Http\Controllers\Settings\PasswordController;
+use App\Http\Controllers\Settings\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth')->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Route::view('settings/profile', 'settings.profile')->name('profile.edit');
+    Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('settings/password', [PasswordController::class, 'edit'])->name('password.edit');
+    Route::put('settings/password', [PasswordController::class, 'update'])->name('password.update');
+
+    Route::view('settings/appearance', 'settings.appearance')->name('appearance');
+});
